fa13f2112be587efb37e065ea48ae3e7d080a980
s0568272 <s0568272@htw-berlin.de>
2020-03-23 10:28:33 +0100

    added new textfield, name input can be printed in toastmessage

--- app/src/main/java/com/example/quokka/FirstFragment.java
+++ app/src/main/java/com/example/quokka/FirstFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -28,6 +30,16 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+        final EditText text_name_field = view.findViewById(R.id.text_name); //musste das Textfeld hier ansprechen, damit es gefunden wird
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = text_name_field.getText().toString();
+                Toast myToast = Toast.makeText(getActivity(), "Hello "+name+"!", Toast.LENGTH_SHORT);
+                myToast.show();
             }
         });
     }
